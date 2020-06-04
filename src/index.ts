65f3f592629a5cf870223ab20164e7d728b8fa61
Gurpreet Singh <gurpreet.singh@preqin.com>
2020-06-04 14:53:44 +0100

    core structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello');
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookiesSession from 'cookie-session';
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(cookiesSession({ keys: ['sdjfhsdkfl'] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('listening on 3000!');
+});
---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,79 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+const requireAuth = (req: Request, res: Response, next: NextFunction): void => {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not pemitted');
+};
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div> 
+        <label> Email </label>
+        <input name="email" />
+      </div>
+      <div> 
+        <label> Password </label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+  if (
+    email &&
+    password &&
+    email === 'test@email.com' &&
+    password === 'password'
+  ) {
+    if (req.session) {
+      req.session.loggedIn = true;
+    }
+
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      Logged in 
+      <a href="/logout"> Log Out </a>
+    `);
+  } else {
+    res.send(`
+      Not logged in 
+      <a href="/login"> Login </a>
+    `);
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.loggedIn = false;
+    res.redirect('/');
+  }
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to classified area! ');
+});
+
+export { router };
